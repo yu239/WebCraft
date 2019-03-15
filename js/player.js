a536a2f660e6c052d9778aee3835281dab555a01
Haonan Yu <haonanyu@fb.com>
2019-03-15 15:32:10 -0700

    debug

--- js/player.js
+++ js/player.js
@@ -72,7 +72,7 @@ Player.prototype.setInputCanvas = function( id )
 
     // once clicking on the canvas, pointer lock is enabled
     canvas.onclick = function() {
-        this.eventHandlers.clickCanvas();
+        player.eventHandlers.clickCanvas();
         canvas.requestPointerLock();
     };
 
